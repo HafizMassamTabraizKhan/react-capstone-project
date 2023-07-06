4893653fdb4e580f073b701bce0cfa1f00b8967d
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-07-06 09:49:41 +0500

    Complete products page

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { BsArrowRightCircle } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import { fetchProducts } from '../redux/products/productsSlice';
 
 function Products() {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products.products);
+  const status = useSelector((state) => state.products.status);
+  const error = useSelector((state) => state.products.error);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  if (status === 'failed') {
+    return (
+      <div>
+        Error:
+        {error}
+      </div>
+    );
+  }
+
   return (
     <>
-      <h1>Products</h1>
+      <div className="search">
+        <input
+          className="input"
+          type="text"
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search by category"
+          value={search}
+        />
+      </div>
+      <div className="products-container">
+        <div className="products-grid">
+          {products
+            .filter((product) => {
+              if (search === '') {
+                return product;
+              }
+              if (product.category.toLowerCase().includes(search.toLowerCase())) {
+                return product;
+              }
+              return null;
+            })
+            .map((product, index) => (
+              <div className={`products-item ${index % 2 === 0 ? 'background-1' : 'background-2'}`} key={product.id}>
+                <img src={product.image} alt="" />
+                <h3>{product.title}</h3>
+                <div className="price-category">
+                  <p>{product.category}</p>
+                  <p>
+                    $
+                    {product.price}
+                  </p>
+                </div>
+                <div className="view-more">
+                  <span>Click for more info</span>
+                  <Link to={`/product/${product.id}`}><BsArrowRightCircle /></Link>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
     </>
   );
 }
--- src/redux/products/productsSlice.js
+++ src/redux/products/productsSlice.js
@@ -1,14 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = {
-  products: [],
-  isLoading: false,
-};
+export const fetchProducts = createAsyncThunk(
+  'products/fetchProducts',
+  async () => {
+    const response = await axios.get('https://fakestoreapi.com/products');
+    return response.data;
+  },
+);
 
 export const productsSlice = createSlice({
   name: 'products',
-  initialState,
+  initialState: {
+    products: [],
+    selectedProduct: null,
+    status: 'idle',
+    error: null,
+  },
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.products = action.payload;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default productsSlice.reducer;
